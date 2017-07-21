b4e4330fd44ff37334c668f364147d9f57942f34
NM.Rabotaev <nm.rabotaev@jet.msk.su>
2017-07-21 17:38:53 +0300

    finished 2.5 - Writing a BeanPostProcessor

---
+++ src/main/java/springjbrains/course/core/lifecycle/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,25 @@
+package springjbrains.course.core.lifecycle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * Created by NM.Rabotaev on 21.07.2017.
+ */
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+    private final static Logger logger = LoggerFactory.getLogger(DisplayNameBeanPostProcessor.class);
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        logger.info(new StringBuilder("In postProcessBeforeInitialization() bean name: ").append(beanName).toString());
+        return null;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        logger.info(new StringBuilder("In postProcessAfterInitialization() bean name: ").append(beanName).toString());
+        return bean;
+    }
+}
